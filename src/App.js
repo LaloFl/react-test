15829bcdafa3b2e0c06cfdc1ac463efadaf62d73
merk <leonardo.mercado193@tectijuana.edu.mx>
2022-01-26 17:04:02 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
-
 import Button from 'components/Button';
 
 function App() {
   return (
     <div className="App">
+      <router> 
+        <Switch>
+          <route>
+            exact 
+            path = "/"
+           
+          </route>
+        </Switch>
+      </router>
       <Button text="soy un botón"></Button>
     </div>
   );
